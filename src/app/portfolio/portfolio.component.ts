193bee6eedbe8de874a4b7c714f830092644a6f9
sayed-joks <sm6502282@gmail.com>
2025-01-10 00:28:53 +0200

    edits

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -7,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './portfolio.component.css'
 })
 export class PortfolioComponent {
-
+  myimg: string[] = [
+      '/images/portfolio/poert1.png',
+      '/images/portfolio/port2.png',
+      '/images/portfolio/port3.png',
+      '/images/portfolio/poert1.png',
+      '/images/portfolio/port2.png',
+      '/images/portfolio/port3.png',
+    ];
+    showlight(image: String): void {
+      let img = document.querySelector('.light') as HTMLElement;
+      img.style.backgroundImage = `url(${image})`;
+      let light = document.querySelector('.light-container') as HTMLElement;
+      light.classList.remove('d-none');
+    }
+    close(): void {
+      let light = document.querySelector('.light-container') as HTMLElement;
+      light.classList.add('d-none');
+    }
+    stopPopgration(e:MouseEvent):void{
+    e.stopPropagation();
+  }
 }
